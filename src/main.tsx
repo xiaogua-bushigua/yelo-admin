9c97c44b1c9c40319387a18bbc21b49fdd9181c9
huanghao <717081557@qq.com>
2023-01-09 18:40:22 +0800

    0.0.1

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/reset.css';
 import '@/assets/styles/global.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter  } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 	</Provider>
 );
